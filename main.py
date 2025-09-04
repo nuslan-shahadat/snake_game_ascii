65c7380a902af9bbb5033ccbae0f820e9d2cdcad
nuslan-shahadat <nuslanshahadat@gmail.com>
2025-09-04 18:09:22 +0600

    added print_grid and mid_index

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-print("hello world")
-print("keep it up")
+import math
+
+ROW = 21
+COLUMN = 21
+
+BORDER_UP = "-"
+BORDER_DOWN = "-"
+BORDER_RIGHT = "|"
+BORDER_LEFT = "|"
+
+BACKGROUND = " ."
+
+GRID = [BACKGROUND for _ in range(ROW*COLUMN)]
+
+def print_grid():
+    grid = GRID
+    for i in range(COLUMN-1):
+        grid.insert((i+1)*ROW+i,"\n")
+    grid =  "".join(grid)
+    print(grid)
+
+
+def mid_index():
+    mid_row = ROW//2
+    return (mid_row*COLUMN)+(mid_row)
